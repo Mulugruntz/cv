2491526defc55015a45214d2c49cdb2046e967f0
Samuel GIFFARD <mulugruntz@gmail.com>
2021-04-26 09:30:19 +0200

    Updated the CV (2021-04-26).

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,12 +1,19 @@
+# exit on error
+set -e
+
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+TODAY=$(date -u '+%Y %m %d')
+
+echo "Root directory defined as $ROOT_DIR"
+mkdir -p "$ROOT_DIR/out"
 
 TEXINPUTS=".:$ROOT_DIR/tex:$TEXINPUTS" xelatex -max-print-line=120 -interaction=nonstopmode -output-directory=$ROOT_DIR/out tex/cv_en_named.tex
 TEXINPUTS=".:$ROOT_DIR/tex:$TEXINPUTS" xelatex -max-print-line=120 -interaction=nonstopmode -output-directory=$ROOT_DIR/out tex/cv_en_anon.tex
 TEXINPUTS=".:$ROOT_DIR/tex:$TEXINPUTS" xelatex -max-print-line=120 -interaction=nonstopmode -output-directory=$ROOT_DIR/out tex/cv_fr_named.tex
 TEXINPUTS=".:$ROOT_DIR/tex:$TEXINPUTS" xelatex -max-print-line=120 -interaction=nonstopmode -output-directory=$ROOT_DIR/out tex/cv_fr_anon.tex
 
-cp "$ROOT_DIR/out/cv_en_named.pdf" "PDF/CV Sam - `date +%Y %m %d`.pdf"
+cp "$ROOT_DIR/out/cv_en_named.pdf" "PDF/CV Sam - $TODAY.pdf"
 cp "$ROOT_DIR/out/cv_en_named.pdf" "CV Samuel GIFFARD.pdf"
 cp "$ROOT_DIR/out/cv_fr_named.pdf" "CV Samuel GIFFARD.fr.pdf"
-cp "$ROOT_DIR/out/cv_en_named.pdf" "Anonymous CV/English.pdf"
-cp "$ROOT_DIR/out/cv_fr_named.pdf" "Anonymous CV/Francais.pdf"
+cp "$ROOT_DIR/out/cv_en_anon.pdf" "Anonymous CV/English.pdf"
+cp "$ROOT_DIR/out/cv_fr_anon.pdf" "Anonymous CV/Francais.pdf"
